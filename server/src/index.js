3374df7717a278afeb92c82be98f320b6fe6912d
udutvic <102656241+udutvic@users.noreply.github.com>
2025-05-08 12:01:56 +0200

    commit

--- server/src/index.js
+++ server/src/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./config/database');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
-  'https:
-  'http:
-  'http:
+  'https://https://crm-repair-node.onrender.com/api',
+  'https://localhost:5000',
+ 
 ];
 app.use(cors({
   origin: allowedOrigins,
