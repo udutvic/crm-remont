fd937923e3549732b016933a1a3ec7596f6a43ca
udutvic <udutvic@gmail.com>
2026-07-31 20:02:54 +0200

    commit 647724de4edd4a608cbb3601a1a46f696ce924df

--- client/src/utils/formatters.ts
+++ client/src/utils/formatters.ts
@@ -2,11 +2,7 @@ export const formatDate = (dateString?: string): string => {
   return dateString ? new Date(dateString).toLocaleDateString("uk-UA") : "-";
 };
 export const formatPrice = (price?: number): string => {
-<<<<<<< HEAD
   return price ? `${price} Kč` : "-";
-=======
-  return price ? `${price} ₴` : "-";
->>>>>>> 647724de4edd4a608cbb3601a1a46f696ce924df
 };
 export const getAvatarUrl = (name: string): string => {
   return `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}`;
